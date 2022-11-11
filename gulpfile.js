148ee70e93e9dd972f83eac06797ce1b1e206597
John J. Jordan <jj@jjjordan.io>
2022-11-11 01:57:47 -0800

    Browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const esbuild = require('gulp-esbuild');
+const browserSync = require('browser-sync');
 
 const tsProject = ts.createProject('tsconfig.json');
+const server = browserSync.create();
 
 gulp.task('ts', () => {
     return gulp.src(['src/*.ts'])
@@ -28,4 +30,30 @@ gulp.task('copydata', () => {
     return gulp.src(['data/registry.json']).pipe(gulp.dest('out/rel/data'));
 });
 
+function serve(done) {
+    server.init({
+        server: {
+            baseDir: 'out/rel',
+        },
+    });
+
+    done();
+}
+
+function notify(done) {
+    server.reload();
+    done();
+}
+
+function watch_ts(done) {
+    return gulp.watch(['src/*.ts'], gulp.series('esbuild', notify));
+}
+
+function watch_html(done) {
+    return gulp.watch(['html/*.html', 'html/*.css'], gulp.series('copyhtml', notify));
+}
+
+gulp.task('watch', gulp.parallel(watch_ts, watch_html));
+
 gulp.task('default', gulp.parallel('esbuild', 'copyhtml', 'copydata'));
+gulp.task('dev', gulp.series('default', serve, 'watch'));
