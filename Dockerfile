fee2ee4b2d644932ccf08e3225544b906b808d02
John J. Jordan <jj@jjjordan.io>
2022-12-08 22:41:44 -0800

    Download registry data at build time if it doesn't exist.

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 FROM node:17 as build
 
 WORKDIR /opt/search
-COPY gulpfile.js package.json package-lock.json /opt/search/
+COPY package.json package-lock.json /opt/search/
 RUN npm install
 
 COPY . /opt/search/
-RUN bzip2 -d /opt/search/data/registry.json.bz2 && ./node_modules/gulp/bin/gulp.js
+RUN npm run gulp rel
 
 FROM nginx:1.23.2
 COPY --from=build /opt/search/out/rel /usr/share/nginx/html
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const esbuild = require('gulp-esbuild');
+const download = require('gulp-download2');
 const browserSync = require('browser-sync');
+const fs = require('fs');
 
 const tsProject = ts.createProject('tsconfig.json');
 const server = browserSync.create();
@@ -22,12 +24,30 @@ gulp.task('esbuild', gulp.series('ts', () => {
         .pipe(gulp.dest('out/rel'));
 }));
 
+gulp.task('esbuild-min', gulp.series('ts', () => {
+    return gulp.src('out/int/js/index.js')
+        .pipe(esbuild({
+            outfile: 'bundle.js',
+            bundle: true,
+            minify: true,
+        }))
+        .pipe(gulp.dest('out/rel'));
+}));
+
 gulp.task('copyhtml', () => {
     return gulp.src(['html/*.css', 'html/*.html']).pipe(gulp.dest('out/rel'));
 });
 
-gulp.task('copydata', () => {
-    return gulp.src(['data/registry.json']).pipe(gulp.dest('out/rel/data'));
+gulp.task('downloaddb', done => {
+    const dburl = 'https://americanpeonysociety.org/cultivars/peony-registry/?registry_data=true';
+    const dboutPath = 'out/rel/data';
+    const dboutName = 'registry.json';
+    const dbout = dboutPath + '/' + dboutName;
+    if (fs.existsSync(dbout)) {
+        done();
+    } else {
+        return download({url: dburl, file: dboutName}).pipe(gulp.dest(dboutPath));
+    }
 });
 
 function serve(done) {
@@ -55,5 +75,6 @@ function watch_html(done) {
 
 gulp.task('watch', gulp.parallel(watch_ts, watch_html));
 
-gulp.task('default', gulp.parallel('esbuild', 'copyhtml', 'copydata'));
+gulp.task('default', gulp.parallel('esbuild', 'copyhtml', 'downloaddb'));
 gulp.task('dev', gulp.series('default', serve, 'watch'));
+gulp.task('rel', gulp.parallel('esbuild-min', 'copyhtml', 'downloaddb'));
